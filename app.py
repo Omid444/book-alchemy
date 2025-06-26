4acf38940e3c2e457b739bc5c0bd90c35c47c8b2
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-06-26 11:17:58 +0200

    absolute path created, connection between Flask app and DB successful

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request
 from data_models import db, Author, Book
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+#os.makedirs("data", exist_ok=True)
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, "data", "library.sqlite")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db.init_app(app)
 
+# with app.app_context():
+#   db.create_all()
 
 
+@app.route('/add_author',Methods=['GET','POST'])
+def handle_add_book():
+    if request.method == 'POST':
 
 
 
--- data_models.py
+++ data_models.py
@@ -1,6 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, INTEGER, Float, String, DATE
+from sqlalchemy import Column, INTEGER, Float, String, ForeignKey
+from sqlalchemy.orm import relationship
+
 db = SQLAlchemy()
+
 
 class Author(db.Model):
     __tablename__ = 'author'
@@ -10,5 +13,18 @@ class Author(db.Model):
     date_of_death = Column(String, nullable=False)
 
 
-    def __str__(self):
+    def __repr__(self):
         return f"id: {Author.id}, {Author.name} {Author.birth_date}  {Author.date_of_death}"
+
+
+class Book(db.Model):
+    __tablename__ = 'book'
+    id = Column(INTEGER, primary_key=True, autoincrement=True)
+    isbn = Column(String, nullable=False)
+    title = Column(String, nullable=False)
+    publication_year = Column(String, nullable=False)
+    author_id = Column(INTEGER, ForeignKey("author.id") )
+
+
+    def __repr__(self):
+        return f"id: {Book.id}, {Book.isbn} {Book.title} {Book.publication_year} {Book.author_id}"
