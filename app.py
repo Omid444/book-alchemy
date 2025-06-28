5cd48c7e3098993cdffafcf6a4b2832c5388066d
Omid444 <86495623+Omid444@users.noreply.github.com>
2025-06-28 18:02:50 +0200

    add book  endpoint and data validation for this endpoint created

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request ,render_template
 from data_models import db, Author, Book
 from flask_sqlalchemy import SQLAlchemy
+
 from datetime import datetime
 import os
 
@@ -14,55 +15,83 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 db.init_app(app)
 
 # with app.app_context():
-#    db.create_all()
+#   db.create_all()
 
-def validate_params(required_keys, received_keys, received_values):
+def validate_params_author(data):
+    message_error = "Parameter should not be left empty. For a live author, write 'live' in date_of_death"
+    required_keys = {'name', 'birth_date', 'date_of_death'}
+    received_keys = set(data.keys())
+
+    print('line 24',received_keys, data.keys(), data.get('name'))
+
+    if received_keys > required_keys:
+        extra = received_keys - required_keys
+        return False, f"Unexpected parameter(s): {', '.join(extra)}"
+
+    elif received_keys < required_keys:
+        missing = required_keys - received_keys
+        return False, f"Missing required parameter(s): {', '.join(missing)}"
+
+    elif received_keys == required_keys:
+        date_format = "%Y-%m-%d"
+
+        try:
+            birth_validation = bool(datetime.strptime(data.get('birth_date'), date_format))
+        except ValueError:
+            return False, "Invalid birth_date format. Please use 'YYYY-MM-DD' format"
+
+        try:
+            date_of_death = data.get('date_of_death')
+            if date_of_death in ('', 'live'):
+                death_validation = True
+            else:
+                death_validation = bool(datetime.strptime(date_of_death, date_format))
+        except ValueError:
+            return False, "Invalid date_of_death format. Please use 'YYYY-MM-DD' format"
+
+        if birth_validation and death_validation:
+            return True, None
+
+        return False, message_error
+    return False, "Unknown validation error"
+
+
+def validate_params_book(data):
 
     message_error = "Parameter should not be left empty"
-
+    required_keys = {'isbn', 'title', 'publication_year'}
+    received_keys = set(request.values.keys())
+    print(received_keys)
     if received_keys > required_keys :
         extra = received_keys - required_keys
         return False, f"Unexpected parameter(s): {', '.join(extra)}"
 
     elif received_keys < required_keys:
-        missing = required_keys - required_keys
-        return False, f"Missing required parameter(s): {missing}"
+        missing = required_keys - received_keys
+        return False, f"Missing required parameter(s): {','.join(missing)}"
 
     elif received_keys == required_keys:
-        date_format = "%d-%m-%Y"
+        year_format = "%Y"
         try:
-            birth_validation = bool(datetime.strptime(received_values[1], date_format))
+            publication_validation = bool(datetime.strptime(data.get('publication_year'), year_format))
         except ValueError:
-            message_error = "Invalid birth_date format, please write valid format '%d-%m-%Y'"
-            birth_validation = False
-        try:
-            if received_values[2] == 'live' or received_values[2] =='':
-                death_validation = True
-            else:
-                death_validation = bool(datetime.strptime(received_values[2], date_format))
-        except ValueError:
-            message_error = "Invalid date_of_death format, please write valid format '%d-%m-%Y'"
-            death_validation = False
-        if all(received_keys) and birth_validation and death_validation:
-            return True , None
+            message_error = ("Invalid publication_year format or empty block , "
+                             "please write valid format e.g.'2004' and fill all required field")
+            return False, message_error
+        if all(request.values.values()) and publication_validation:
+            return True, None
 
         return False, message_error
-
-
-
+    return False, "Unknown validation error"
 
 
 
 @app.route('/add_author', methods=['GET','POST'])
 def handle_add_author():
     if request.method == 'POST':
-
-        required_keys = {'name', 'birth_date', 'date_of_death'}
-        received_keys = set(request.values.keys())
-        received_values = list(request.values.values())
-        data_validation, message_error = validate_params(required_keys, received_keys, received_values)
-        print(data_validation, message_error)
-        print(received_keys, received_values)
+        print('request value',request.values)
+        data_validation, message_error = validate_params_author(request.values)
+        print('data validation',data_validation, message_error)
         if data_validation:
             name = request.values.get('name')
             birth_date = request.values.get('birth_date')
@@ -87,19 +116,34 @@ def handle_add_author():
 @app.route('/add_book',methods=['GET','POST'])
 def handle_add_book():
     if request.method == 'POST':
-        title = request.values.get('title')
-        isbn = request.values.get('isbn')
-        publication_year = request.values.get('publication_year')
+        print(request.values)
+        data_validation, message_error = validate_params_book(request.values)
+        print(data_validation, message_error)
+        if data_validation:
+            isbn = request.values.get('isbn')
+            title = request.values.get('title')
+            publication_year = request.values.get('publication_year')
+            print(isbn, title, publication_year)
+            book = Book(isbn=isbn, title=title, publication_year=publication_year)
+            db.session.add(book)
+            db.session.commit()
+            return render_template('add_book.html', message=f'Book {title} successfully added to DB')
+
+        else:
+            return render_template('add_book.html', message=message_error)
+
+    # if request is GET but there are extra params there
+    if request.args:
+        message_error = "Extra parameter are not allowed in GET request. please correct URL address"
+        return render_template('add_book.html', message=message_error)
+
+    return render_template('add_book.html')
 
 
-
-
-
-
-
-
-
-
+@app.route('/',methods=['GET'])
+def handle_home_page():
+    books = S
+    return render_template('home.html', books=books)
 
 
 if __name__ == "__main__":
